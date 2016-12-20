cce08b934c755a363f54d940e2c71b1284b8d94b
Eric Bomblatus <ebomblatus@gmail.com>
2016-12-20 12:07:40 -0800

    write app.rb

--- app.rb
+++ app.rb
@@ -5,3 +5,41 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get ('/store_info') do
+  @all_stores = Store.all()
+  erb(:store_info)
+end
+
+post ('/store_info/new_store') do
+  name_input = params.fetch('name_input')
+  @new_store = Store.create({name: name_input})
+end
+
+get('store_info/store_form/:id') do
+  @current_store = Store.find(params[:id].to_i)
+  @current_store_brans = @current_store.brands
+  @all_brands = Brand.all
+  erb(:store_form)
+end
+
+get ('/brand_info') do
+  @all_brands = Brand.all()
+  erb(:brand_info)
+end
+
+post ('/brand_info/new_brand') do
+  name_input = params.fetch('name_input')
+  @new_brand = Brand.create({name: name_input})
+end
+
+get ('brand_info/brand_form/:id') do
+  @current_brand = Brand.find(params[:id].to_i)
+  erb(:brand_form)
+end
+
+delete('store_form/:id/delete') do
+  @current_store = Store.find(params[:id])
+  @current_store.destroy
+  redirect('/store_info')
+end 
